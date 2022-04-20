f1fa5742cf24dda3a0a0e837d0b0ae00c26527fb
kunaljha12 <kunaljha004@gmail.com>
2022-04-20 13:41:39 +0530

    Commited

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -3,8 +3,10 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-property-card',
-  template: `<h1>I am a card</h1>`
-
+  // template: `<h1>I am a card</h1>`,
+  templateUrl: "property-card.component.html",
+  // styles: ['h1 {font-weight: normal;}']
+  styleUrls: ["property-card.component.css"]
 }
 )
 export class PropertyCardComponent {
