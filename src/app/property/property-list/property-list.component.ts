cf50bd0ff9d2195cc75a8d7316df39826d977457
kunaljha12 <kunaljha004@gmail.com>
2022-04-22 11:03:31 +0530

    cmt

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HousingService } from 'src/app/services/housing.service';
 
 @Component({
   selector: 'app-property-list',
@@ -7,15 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./property-list.component.css']
 })
 export class PropertyListComponent implements OnInit {
-  properties: any;
+  properties: Array<any>;
 
-  constructor(private http:HttpClient) { }
+  constructor(private housingService:HousingService) { }
 
   ngOnInit(): void{
-    this.http.get('data/properties.json').subscribe(
+    this.housingService.getAllProperties().subscribe(
       data=>{
         this.properties = data;
         console.log(data);
+      }, error =>{
+        console.log('httperror: ');
+        console.log(error);
       }
     );
   }
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllProperties(){
+    return this.http.get('data/properties.json').pipe(
+      map(data =>{
+        const propertiesArray: Array<any> = [];
+        for(const id in data){
+          if(data.hasOwnProperty(id)){
+            propertiesArray.push(data[id]);
+          }
+        }
+        return propertiesArray;
+      })
+    );
+  }
 }
