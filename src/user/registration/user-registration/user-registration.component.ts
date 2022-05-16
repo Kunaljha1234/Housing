8ef5a14300aba12f271ffb0c3b1f699a97abe12e
Kunal Jha <kunaljha004@gmail.com>
2022-05-16 16:26:05 +0530

    registration form completed

--- src/user/registration/user-registration/user-registration.component.ts
+++ src/user/registration/user-registration/user-registration.component.ts
@@ -16,12 +16,34 @@ export class UserRegistrationComponent implements OnInit {
       userName: new FormControl(null, Validators.required),
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-      cpassword: new FormControl(null, Validators.required),
+      cpassword: new FormControl(null, [Validators.required]),
       mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
     },this.passwordMatchingValidators);
   }
   passwordMatchingValidators(fg: FormGroup): Validators{
     return fg.get('password').value === fg.get('cpassword').value ? null:{notmatched: true};
+
+  }
+  // getter methods for form control
+
+  get userName(){
+    return this.registrationForm.get('userName') as FormControl
+  }
+
+  get email(){
+    return this.registrationForm.get('email') as FormControl
+  }
+
+  get password(){
+    return this.registrationForm.get('password') as FormControl
+  }
+
+  get cpassword(){
+    return this.registrationForm.get('cpassword') as FormControl
+  }
+
+  get mobile(){
+    return this.registrationForm.get('mobile') as FormControl
   }
   onSubmit(){
     console.log(this.registrationForm);
